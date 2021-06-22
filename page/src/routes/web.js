0df674fb252d32058375685468d9c48810e4c337
pedroKahane <pedrok@datosclaros.com>
2021-06-22 10:31:44 -0300

    agregue estructura de html

--- page/src/routes/web.js
+++ page/src/routes/web.js
@@ -9,5 +9,8 @@ router.get("/login", (req,res) => res.sendFile(path.resolve(__dirname, "../views
 router.get("/register", (req,res) => res.sendFile(path.resolve(__dirname, "../views", "register.html")))
 router.get("/productDetail", (req,res) => res.sendFile(path.resolve(__dirname, "../views", "productDetail.html")))
 router.get("/productCart", (req,res) => res.sendFile(path.resolve(__dirname, "../views", "productCart.html")))
+router.get("/contacto", (req,res) => res.sendFile(path.resolve(__dirname, "../views", "contacto.html")))
+router.get("/quienesSomos", (req,res) => res.sendFile(path.resolve(__dirname, "../views", "quienesSomos.html")))
+router.get("/tienda", (req,res) => res.sendFile(path.resolve(__dirname, "../views", "tienda.html")))
 
 module.exports = router
