16117ad2d73f8a7b5941ccaf888c26d0dc15c66f
PedroKahane <pedrok@datosclaros.com>
2021-07-01 12:39:26 -0300

    agregue sistema mvc

---
+++ page/src/routes/main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const main = require("../controllers/main")
+
+router.get("/", main.index)
+router.get("/login", main.login)
+router.get("/register", main.register)
+router.get("/productDetail", main.productDeatil)
+router.get("/productCart", main.productCart)
+router.get("/productCartDos", main.productCartDos)
+router.get("/productCartTres", main.productCartTres)
+router.get("/contacto", main.contacto)
+router.get("/tienda", main.tienda)
+
+
+
+
+module.exports = router
