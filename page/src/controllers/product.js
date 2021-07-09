e77395cbea02656f4ada356d8647ecb6549ab9da
pedroKahane <pedrok@datosclaros.com>
2021-07-09 20:27:10 -0300

    map sobre talles

--- page/src/controllers/product.js
+++ page/src/controllers/product.js
@@ -2,6 +2,6 @@ const path = require('path');
 const product = require("../models/product")
 
 module.exports = {
-    tienda:(req,res) => res.send({styles:"tienda.css", products: product.allWithExtra()}),
+    tienda:(req,res) => res.render("tienda", {styles:"tienda.css", products: product.allWithExtra()}),
     product:(req,res) => res.render("productDetail",{styles:"productDetail.css"})
 }
