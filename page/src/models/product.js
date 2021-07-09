11b29547964d8c29ca3102d5561237d98e209979
pedroKahane <pedrok@datosclaros.com>
2021-07-09 19:04:17 -0300

    map sobre talles

--- page/src/models/product.js
+++ page/src/models/product.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const fs = require('fs');
-const colorModel = require('./color.js');
+const color = require('./color.js');
 const equipos = require('./equipos.js');
-const talle = require('./talles.js');
+const talleModel = require('./talles.js');
 const model = {
     directory : path.resolve(__dirname,"../data","productos.JSON"),
     all: function() {
@@ -14,6 +14,17 @@ const model = {
         let productos = this.all();
         productos.map(element => {
             element.equipos = equipos.one(element.equipos);
+            return element
+        })
+        productos.map(element => {
+            element.talles = element.talles.map(talle => {
+                talle = talleModel.one(talle)
+                return talle
+            })
+            return element
+        })
+        productos.map(element => {
+            element.color = color.one(element.color);
             return element
         })
         return productos 
