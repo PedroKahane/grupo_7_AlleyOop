9145e88127ea1e3d1bc9fff3bfeb6b0d8e371368
sebastiansalvat <sebastianestebansalvat@gmail.com>
2021-07-13 12:42:59 -0300

    correciones

--- page/src/controllers/product.js
+++ page/src/controllers/product.js
@@ -2,6 +2,6 @@ const path = require('path');
 const product = require("../models/product")
 
 module.exports = {
-    tienda:(req,res) => res.render("tienda", {styles:"tienda.css", products: product.allWithExtra()}),
-    product:(req,res) => res.render("productDetail",{styles:"productDetail.css", product: product.oneWithExtra(req.params.id)})
+    tienda:(req,res) => res.render("products/tienda", {styles:"tienda.css", products: product.allWithExtra()}),
+    product:(req,res) => res.render("products/productDetail",{styles:"productDetail.css", product: product.oneWithExtra(req.params.id)})
 }
