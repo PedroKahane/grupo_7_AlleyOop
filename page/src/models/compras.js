061db4de1066722a633fc83b4636d39d354a6de4
PedroKahane <pedrok@datosclaros.com>
2021-08-19 17:08:27 -0300

    carrito para un producto

--- page/src/models/compras.js
+++ page/src/models/compras.js
@@ -17,10 +17,49 @@ module.exports = {
         let nuevo = {
             id: compras.length > 0 ? compras[compras.length -1].id + 1: 1,
             productoId : data.id,
-            cantidad : data.cantidad
+            cantidad : data.cantidad,
+            precio : data.precio,
+            precioTotal: parseInt((data.precio * data.cantidad))
         }    
         compras.push(nuevo)
         this.write(compras);
         return nuevo;    
     },
+    productoCartDos : function (data, id){
+        let compras = this.all();;
+        compras.map(compra => {
+            if(compra.id == id ){
+                compra.nombre = data.nombre,
+                compra.apellido = data.apellido,
+                compra.email = data.email,
+                compra.telefono = data.telefono,
+                compra.direccion = data.direccion,
+                compra.localidad = data.localidad,
+                compra.provincia = data.provincia,
+                compra.codigopostal = data.codigopostal
+                return compra
+            }
+            return compra
+        })
+        this.write(compras)
+        return true;
+    },
+    productoCartTres : function (data, id){
+        let compras = this.all();;
+        compras.map(compra => {
+            if(compra.id == id ){
+                compra.metododepago = data.metododepago,
+                compra.numerotarjeta = data.numerotarjeta,
+                compra.vencimiento = data.vencimiento,
+                compra.cvv = bcrypt.hashSync(data.cvv,10)
+            }
+            return compra
+        })
+        this.write(compras)
+        return true;
+    },
+    ultimoId : function(){
+        let compras = this.all();
+        return compras[compras.length -1].id
+    }
 }
--- page/src/routes/main.js
+++ page/src/routes/main.js
@@ -9,6 +9,8 @@ router.get("/productCart", authMiddleware, main.productCart)
 router.get("/productCartDos",authMiddleware, main.productCartDos)
 router.post("/agregarCarrito/:id",authMiddleware,controller.comprarProducto);
 router.post("/productCart", authMiddleware, main.productCartpaso)
+router.put("/productCartDos", authMiddleware, main.productCartpasoDos)
+router.put("/productCartTres", authMiddleware, main.productCartpasoTres)
 router.get("/productCartTres",authMiddleware, main.productCartTres)
 router.get("/contacto", main.contacto)
 
