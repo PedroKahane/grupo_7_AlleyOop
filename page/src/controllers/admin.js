fffc09923f6b4241b34ecd52066fc0556a68b9c9
PedroKahane <pedrok@datosclaros.com>
2021-08-20 22:41:17 -0300

    primer boceto admin

--- page/src/controllers/admin.js
+++ page/src/controllers/admin.js
@@ -5,4 +5,7 @@ module.exports = {
     admin:(req,res) => res.render("admin/admin",{styles:"admin.css"}),
     products:(req,res) => res.render("admin/products",{styles:"adminProduct.css",products: product.allWithExtra()}),
     ventas:(req,res) =>  res.render("admin/ventas",{styles:"ventas.css",compras: compras.allWithExtra()}),
+    ventasCanceladas:(req,res) =>  res.render("admin/ventasCanceladas",{styles:"ventas.css",compras: compras.allWithExtra()}),
+    ventasFinalizadas:(req,res) =>  res.render("admin/ventasFinalizadas",{styles:"ventas.css",compras: compras.allWithExtra()}),
 }
+
--- page/src/routes/admin.js
+++ page/src/routes/admin.js
@@ -9,5 +9,7 @@ const userAdmin = require('../middlewares/userAdmin')
 router.get("/admin" ,userAdmin, controller.admin)
 router.get("/products" ,userAdmin, controller.products)
 router.get("/ventas" ,userAdmin, controller.ventas)
+router.get("/ventasCanceladas" ,userAdmin, controller.ventasCanceladas)
+router.get("/ventasFinalizadas" ,userAdmin, controller.ventasFinalizadas)
 
 module.exports = router
