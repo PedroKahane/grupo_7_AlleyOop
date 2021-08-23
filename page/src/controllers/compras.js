0c51d260a75b8fd0d7353420a0166b20be3bdd87
sebastiansalvat <sebastianestebansalvat@gmail.com>
2021-08-23 14:59:49 -0300

    validaciones carrito

--- page/src/controllers/compras.js
+++ page/src/controllers/compras.js
@@ -1,8 +1,6 @@
 const user = require("../models/user")
 const comprasModel = require("../models/compras");
-
-
-;
+const { validationResult } = require('express-validator');
 
 module.exports = {
     productCart:(req,res) => {
@@ -15,10 +13,30 @@ module.exports = {
         return res.redirect("/compras/productCartDos")
     },
     productCartpasoDos: (req,res) => {
+        const resultValidation = validationResult(req);
+
+        if (!resultValidation.isEmpty()) {
+            return res.render('carrito/productCartDos', {
+                styles:"productCart.css", 
+                compras: comprasModel.one(comprasModel.ultimoId()), 
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+        }
         result = comprasModel.productoCartDos(req.body, req.body.id)
         return res.redirect("/compras/productCartTres")
     },
-    productCartpasoTres : (req,res) => {
+    productCartpasoTres : (req,res) => {  
+        const resultValidation = validationResult(req);
+
+        if (!resultValidation.isEmpty()) {
+            return res.render('carrito/productCartTres', {
+                styles:"productCart.css", 
+                compras: comprasModel.oneWithExtra(comprasModel.ultimoId()), 
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+        }     
         result = comprasModel.productoCartTres(req.body, req.body.id)
         return res.redirect("/compras/compraExitosa")
     },
--- page/src/routes/compras.js
+++ page/src/routes/compras.js
@@ -1,7 +1,39 @@
 const express = require('express');
+const { body } = require("express-validator");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware")
 const compras = require("../controllers/compras")
+
+// Validaciones con express-validator
+const validacionesDos = [
+    body('nombre').notEmpty().withMessage('Tenés que ingresar un nombre'),
+    body('apellido').notEmpty().withMessage('Tenés que ingresar un apellido'),
+    body('email').notEmpty().withMessage('Tenés que ingresar un correo electrónico').bail()
+                  .isEmail().withMessage('Debes ingresar un formato de correo válido'),
+    body('telefono').notEmpty().withMessage('Tenés que ingresar un numero telefónico').bail()
+                    .isMobilePhone().withMessage('Tiene que ser un numero de telefono válido'),
+    body('direccion').notEmpty().withMessage('Tenés que ingresar una direccion de envío').bail()
+                    .isAlphanumeric().withMessage('Tenés que ingresar una dirección correcta'),
+    body('localidad').notEmpty().withMessage('Tenés que ingresar una localidad'),
+    body('provincia').notEmpty().withMessage('Tenés que ingresar una provincia'),
+    body('codigopostal').notEmpty().withMessage('Tenés que ingresar un código postal').bail()
+                        .isNumeric().withMessage('Debe ser un número de 4 digitos').bail()
+                        .isLength({min:4, max:4}).withMessage('Debe ser un número de 4 digitos'),
+  ];
+
+  const validacionesTres = [
+    body('numerotarjeta').notEmpty().withMessage('Tenés que ingresar un número').bail()
+                        .isNumeric().withMessage('Debe ser un número de tarjeta válido').bail()
+                        .isLength({min:16, max:16}).withMessage('Debe contener 16 digitos'),
+    body('vencimiento').notEmpty().withMessage('Tenés que ingresar una fecha').bail()
+                        .isDate().withMessage('Tenés que ingresar una fecha válida'),
+    body('metododepago').notEmpty().withMessage('Debes seleccionar el tipo de tarjeta'),
+    body('cvv').notEmpty().withMessage('Tenés que ingresar un número').bail()
+                        .isNumeric().withMessage('Debe ser un número de 3 digitos').bail()
+                        .isLength({min:3, max:3}).withMessage('Debe ser un número de 3 digitos')
+  ]
+
+
 
 router.get("/productCart", authMiddleware, compras.productCart)
 router.get("/productCartDos",authMiddleware, compras.productCartDos)
@@ -9,8 +41,8 @@ router.get("/productCartTres",authMiddleware, compras.productCartTres)
 router.get("/compraExitosa",authMiddleware, compras.compraExitosa)
 router.post("/agregarCarrito/:id",authMiddleware,compras.comprarProducto);
 router.post("/productCart", authMiddleware, compras.productCartpaso)
-router.put("/productCartDos", authMiddleware, compras.productCartpasoDos)
-router.put("/productCartTres", authMiddleware, compras.productCartpasoTres)
+router.put("/productCartDos", [authMiddleware,validacionesDos], compras.productCartpasoDos)
+router.put("/productCartTres", [authMiddleware,validacionesTres], compras.productCartpasoTres)
 
 
 module.exports = router
