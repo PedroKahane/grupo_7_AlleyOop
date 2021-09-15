606528f30633f355560cd6c4070f1ee36e4af25f
sebastiansalvat <sebastianestebansalvat@gmail.com>
2021-09-15 11:04:13 -0300

    Css Vistas

--- page/src/controllers/main.js
+++ page/src/controllers/main.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const db = require('../database/models');
 const product = require("../models/product")
 
 ;
 
 module.exports = {
-    index:(req,res) => res.render("index",{styles:"styles.css",  products: product.allWithExtra()}),
+    index: async (req,res) => {
+        try {
+            let product = await db.Product.findAll({where: {destacado: 1}})
+            res.render("index",{styles:"styles.css",  products: product })
+        } catch (error) {
+        console.log(error);
+        }
+    },
     contacto:(req,res) => res.render("contacto",{styles:"contacto.css", }),
 }
