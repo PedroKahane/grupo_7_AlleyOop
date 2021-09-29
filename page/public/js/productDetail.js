dd104356fbfee59ae5ffd6268630ae60092a12a2
pedroKahane <pedrok@datosclaros.com>
2021-09-29 13:51:27 -0300

    termine galeria product detail

--- page/public/js/productDetail.js
+++ page/public/js/productDetail.js
@@ -1,13 +1,16 @@
 let frente = document.querySelector(".frente")
 let espalda = document.querySelector(".espalda")
-let frenteSrc = document.querySelector(".frente").src
-let espaldaSrc = document.querySelector(".espalda").src
+let frenteSrc = document.querySelector(".frente").getAttribute("src")
+let espaldaSrc = document.querySelector(".espalda").getAttribute("src")
 let contenedor = document.querySelector(".contenedor_img")
 let original = document.querySelector(".original")
 const img = document.querySelector("#producto_principal")
 const icon = document.getElementById("cancel_icon")
+let imagenes = []
 
+imagenes.push(frenteSrc, espaldaSrc)
 
+console.log(imagenes);
 frente.addEventListener("click", function() {
     const producto_principal = document.querySelector("#producto_principal")
     producto_principal.src = frenteSrc
@@ -40,4 +43,54 @@ window.addEventListener("keydown", evento => {
         contenedor.classList.remove("show")
         contenedor.classList.add("none")
     }
+})
+
+
+const arrows = document.querySelectorAll(".arrows")
+
+arrows.forEach((arrow) => {
+    arrow.addEventListener("click", evento => {
+        const target = evento.target
+        const actual = document.querySelector(".contenedor_img img")
+        const source = actual.getAttribute("src")
+        if(target.className.includes("right")){
+            const indice = imagenes.indexOf(source)
+            if(indice < imagenes.length -1){
+                actual.setAttribute("src", imagenes[indice + 1])
+            } else{
+                actual.setAttribute("src", imagenes[0])
+            }
+        }
+        if(target.className.includes("left")){
+            const indice = imagenes.indexOf(source)
+            if(indice > 0){
+                actual.setAttribute("src", imagenes[indice - 1])
+            } else{
+                actual.setAttribute("src", imagenes[imagenes.length -1 ])
+            }
+        }
+    })
+})
+
+
+window.addEventListener("keydown", evento => {
+    const target = evento.target
+    const actual = document.querySelector(".contenedor_img img")
+    const source = actual.getAttribute("src")
+    const key = evento.key
+    if(key == "ArrowRight" && contenedor.classList.contains("show")){
+        const indice = imagenes.indexOf(source)
+            if(indice < imagenes.length -1){
+                actual.setAttribute("src", imagenes[indice + 1])
+            } else{
+                actual.setAttribute("src", imagenes[0])
+            }
+    } else if(key == "ArrowLeft" && contenedor.classList.contains("show")){
+        const indice = imagenes.indexOf(source)
+            if(indice > 0){
+                actual.setAttribute("src", imagenes[indice - 1])
+            } else{
+                actual.setAttribute("src", imagenes[imagenes.length -1 ])
+            }
+        }
 })
