d749ad464f3f83fde28e78f9020c4d6927e20b40
pedroKahane <pedrok@datosclaros.com>
2021-09-29 16:19:17 -0300

    termine dropdown perfil con transition

--- page/public/js/dropdown.js
+++ page/public/js/dropdown.js
@@ -2,13 +2,17 @@ let dorpdown = document.querySelector(".dropdown")
 let profile = document.querySelector("#a_img_log")
 let bars = document.querySelector(".burger-menu")
 let menu = document.querySelector("#menu_header")
-
+window.addEventListener("load", function(){
+  dorpdown.classList.remove('none');
+})
 
 profile.addEventListener("click", function(){
-    if (dorpdown.classList.contains('none')) {
-        dorpdown.classList.remove('none');
+    if (dorpdown.classList.contains('hidden')) {
+        dorpdown.classList.remove('hidden');
+        dorpdown.classList.add('show')
       }else{
-        dorpdown.classList.add('none')
+        dorpdown.classList.add('hidden');
+        dorpdown.classList.remove("show")
       }
 })
 
