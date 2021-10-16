de97204f974f7e03592c3f5513eeb18fa9696e30
pedroKahane <pedrok@datosclaros.com>
2021-10-16 17:02:04 -0300

    agregue ultimas 5 ventas

--- react/dashboard/src/pages/productos.js
+++ react/dashboard/src/pages/productos.js
@@ -1,10 +1,6 @@
 
 import '../App.css';
 
-<<<<<<< HEAD
-=======
-//import productsByColor from '../components/productsByColor';
->>>>>>> 23d7904ba0a70543d653602276d12ed7d9cef051
 
 
 function Products() {
